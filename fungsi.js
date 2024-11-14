474767f9a0cc96b4e3f55a1ca9e6777f08a0a30f
HervinWIlen <wilen.hervin@gamil.com>
2024-11-14 15:44:46 +0700

    perubahan komir ec99cd6 coba revert

--- fungsi.js
+++ fungsi.js
@@ -4,8 +4,12 @@ console.log(add(1, 2));
 const substact = (a, b) => a - b;
 console.log(substact(5, 2));
 
+<<<<<<< HEAD
 const multiply = (a, b) => a * b;
 console.log(multiply(4, 2));
 
 const divide = (a, b) => a / b;
 console.log(divide(8, 2));
+=======
+
+>>>>>>> parent of ec99cd6 (Penambahan fungsi perkalian)
